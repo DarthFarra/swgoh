1e78b1845be92d183ba96aac55ed46ffb12e7150
DarthFarra <rafagarciaplanas@gmail.com>
2026-05-22 17:28:43 +0200

    Update main_bot.py

--- src/swgoh/bot/main_bot.py
+++ src/swgoh/bot/main_bot.py
@@ -29,7 +29,7 @@ from apscheduler.triggers.cron import CronTrigger
 from telegram.ext import ApplicationBuilder
 
 from .config import BOT_TOKEN, SEND_ASSIGNMENTS_TIME, SYNC_GUILDS_CRON, SYNC_DATA_CRON, TIMEZONE
-from .commands import syncguild, misoperaciones, register, syncdata, operacionesjugador, tickets, sendassignments, omicrones, tb, tb_notifications,)
+from .commands import syncguild, misoperaciones, register, syncdata, operacionesjugador, tickets, sendassignments, omicrones, tb, tb_notifications
 from .discord_listener import start_discord_listener, stop_discord_listener
 from .jobs.snapshot_tickets import schedule_snapshot_jobs
 from .jobs.send_assignments_daily import job_send_assignments
